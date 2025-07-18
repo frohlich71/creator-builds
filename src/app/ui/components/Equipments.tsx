58898d3fc1788ec4203df916fb9d10b7c12809f8
frohlich71 <victorfrohlich520@gmail.com>
2025-07-18 13:04:55 -0300

    feat: visual adjuspments

--- src/app/ui/components/Equipments.tsx
+++ src/app/ui/components/Equipments.tsx
@@ -66,7 +66,7 @@ export default function Equipments({equipments} : {equipments: Equipment[]}) {
               >
                 <ArrowTopRightOnSquareIcon className="w-3 h-3 md:w-4 md:h-4 mr-1 md:mr-2" />
                 <span className="hidden sm:inline">View Product</span>
-                <span className="sm:hidden">View Product</span>
+                <span className="sm:hidden">View</span>
               </a>
             </div>
           </div>
